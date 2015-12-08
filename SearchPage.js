be7cef22013d4ae09e0b9584a53d1d0222384347
Edgar Pabon <edgar.pabon@gmail.com>
2015-12-07 21:15:58 -0800

    Added page for Property View

--- SearchPage.js
+++ SearchPage.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var React = require('react-native');
+var SearchResults = require('./SearchResults');
 
 var {
   StyleSheet,
@@ -118,14 +119,33 @@ class SearchPage extends Component {
   }
 
   onSearchPressed() {
-    var query = urlForQueryAndPage('place-name', this.state.searchString, 1);
+    var query = urlForQueryAndPage('place_name', this.state.searchString, 1);
     this._executeQuery(query);
+  }
+
+  onLocationPressed() {
+    navigator.geolocation.getCurrentPosition(
+      location => {
+        var search = location.coords.latitude + ',' + location.coords.longitude;
+        this.setState({ searchString: search });
+        var query = urlForQueryAndPage('centre_point', search, 1);
+        this._executeQuery(query);
+      },
+      error => {
+        this.setState({
+          message: 'There was a problem with obtaining your location: ' + error
+        });
+      });
   }
 
   _handleResponse(response) {
     this.setState({ isLoading: false , message: '' });
     if (response.application_response_code.substr(0,1) === '1') {
-      console.log('Properties found: ' + response.listings.length);
+      this.props.navigator.push({
+        title: 'Results',
+        component: SearchResults,
+        passProps: {listings: response.listings}
+      });
     } else {
       this.setState({ message: 'Location not recognized; please try again.'});
     }
@@ -154,7 +174,7 @@ class SearchPage extends Component {
           </TouchableHighlight>
         </View>
 
-        <TouchableHighlight style={styles.button} underlayColor='#99d9f4'>
+        <TouchableHighlight style={styles.button} underlayColor='#99d9f4' onPress={this.onLocationPressed.bind(this)}>
           <Text style={styles.buttonText}>Location</Text>
         </TouchableHighlight>
         <Image source={require('./ios/PropertyFinder/Images.xcassets/house.imageset/house.png')} style={styles.image}/>
